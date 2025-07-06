0dbebc7ae2dd11949cc76c562726b11626fd593f
Djsilvertail <djsilvertail76@gmail.com>
2025-07-06 07:55:57 -0700

    Fixed file issues

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/calibrated-auth', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
