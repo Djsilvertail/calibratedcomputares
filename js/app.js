e25261c2f6ac7780992e22e17d591a43ec27f622
Djsilvertail <djsilvertail76@gmail.com>
2025-07-06 13:47:39 -0700

    Fixed some css, and js

--- js/app.js
+++ js/app.js
@@ -43,7 +43,9 @@ const reviews = [
 ];
 
 // Public pages
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  res.render('index', { req });
+});
 app.get('/about', (req, res) => res.render('about'));
 app.get('/services', (req, res) => res.render('services'));
 app.get('/reviews', (req, res) => res.render('reviews', { reviews }));
