1980984463205dd4b7f22f02fed560dc3a00b6dc
Djsilvertail <djsilvertail76@gmail.com>
2025-07-12 15:31:06 -0700

    weird

--- js/app.js
+++ js/app.js
@@ -240,5 +240,5 @@ app.get('/logout', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
